4176afaae454c1840041d6b72515785cd8261df2
Julia Hardy <julia.joanna.hardy@gmail.com>
2019-10-11 10:53:48 +0200

    add local storage

--- Book-List/appes6.js
+++ Book-List/appes6.js
@@ -43,6 +43,44 @@ class UI {
     }
 }
 
+class Store {
+    static getBooks(){
+        let books;
+        if(localStorage.getItem('books') === null){
+            books = [];
+        } else {
+            books = JSON.parse(localStorage.getItem('books'));
+        }
+        return books;
+    }
+    static displayBooks() {
+        const books = Store.getBooks();
+        books.forEach(function(book){
+            const ui = new UI();
+            ui.addBookToList(book);
+        });
+
+    }
+    static addBook(book){
+        const books = Store.getBooks();
+        books.push(book);
+
+        localStorage.setItem('books', JSON.stringify(books));
+
+    }
+    static removeBook(isbn){
+        const books = Store.getBooks();
+        books.forEach(function(book, index){
+            if(book.isbn === isbn){
+                books.splice(index, 1);
+            }
+        });
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+}
+
+document.addEventListener('DOMContentLoaded', Store.displayBooks);
+
 document.getElementById('book-form').addEventListener('submit', function(e){
     const title = document.getElementById('title').value;
     const author = document.getElementById('author').value;
@@ -55,6 +93,7 @@ document.getElementById('book-form').addEventListener('submit', function(e){
             ui.showAlert('Please fill in all fields', 'error');
     } else {
             ui.addBookToList(book);
+            Store.addBook(book);
             ui.showAlert('Book Added!', 'success');
             ui.clearFields(); 
     }
@@ -64,6 +103,7 @@ document.getElementById('book-form').addEventListener('submit', function(e){
 document.getElementById('book-list').addEventListener('click', function(e){
     const ui = new UI();
     ui.deleteBookFromList(e.target);
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
     ui.showAlert('Book Removed!', 'success');
 
     e.preventDefault();
